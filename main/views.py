9ed92cacc200d10e8de11385d7a93b542c3cb280
Aaron-gl94 <aaron_admb94@hotmail.com>
2016-08-09 13:57:13 -0500

    Creción de vista y template home

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+class HomeView(View):
+	def get(self, request):
+		template_name = 'home.html'
+		return render(request, template_name)
