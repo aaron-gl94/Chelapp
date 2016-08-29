1c00f86be9b19a924e5dba24131f9d08daa04378
Aaron-gl94 <aaron_admb94@hotmail.com>
2016-08-29 18:09:22 -0500

    Accounts

--- Chela/urls.py
+++ Chela/urls.py
@@ -1,9 +1,18 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from main import views
 from django.conf import settings
+from accounts import urls as AccUrls
+
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',views.HomeView.as_view(),name="home"),
+    url(r'^accounts/', include(AccUrls)),
+    url('',include('social.apps.django_app.urls',namespace="social")),
+    url(
+        regex=r'^media/(?P<path>.*)$',
+        view='django.views.static.serve',
+        kwargs={'document_root':settings.MEDIA_ROOT}),
 ]
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+
+class Profile(models.Model):
+	user = models.OneToOneField(settings.AUTH_USER_MODEL)
+	tel = models.CharField(max_length=10, blank=True, null=True)
+	date_of_birth = models.DateField(blank=True,null=True)
+	photo = models.ImageField(upload_to="users/%Y/%m/%d",blank=True)
+
+
+	def __str__(self):
+		return 'Perfil del usuario {}'.format(self.user.username)
