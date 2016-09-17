9f6b98c03e5a92cbd7d6bb24ffbf2fbc28513456
luis92arma <luis.arma92@gmail.com>
2016-09-16 20:03:57 -0500

    catalogo con filtros y se midifico base.html

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^category/(?P<category>[\w-]+)/$', views.ProductListView.as_view(), name='list_filter'),
     url(r'^detalle/(?P<slug>[\w-]+)/$', views.ProductDetailView.as_view(), name='detalle'),
     url(r'^', views.ProductListView.as_view(), name='products'),
 
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
-from .models import Product
+from .models import Product, Category
 # Create your views here.
 
 class ProductListView(View):
-    def get(self, request):
+    def get(self, request, category=None):
         template_name = 'catalogo/list_product.html'
         products = Product.objects.all()
+        tag = None
+        if category:
+            tag = Category.objects.get(name_category=category)
+            products = Product.objects.all().filter(category=tag)
+        #cat = Category.objects.all()
         context = {
             'products':products,
+            #'tag':tag,
+            #'cat':cat,
         }
         return render(request, template_name, context)
 
